4ae7e9f9aaaac709e258dc422d87f6bb43859126
yamatowani <festajaune.18637@icloud.com>
2025-05-05 15:51:23 +0900

    add: reverseArrayInPlace

--- exercise04/reverse.js
+++ exercise04/reverse.js
@@ -8,3 +8,13 @@ function reverseArray(arr) {
  
 console.log(reverseArray([1,2,3]))
 
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    temp = arr[i]
+    arr[i] = arr[arr.length - 1]
+    arr[arr.length - 1] = temp
+  }
+  return arr
+}
+
+console.log(reverseArrayInPlace([1,2,3]))
