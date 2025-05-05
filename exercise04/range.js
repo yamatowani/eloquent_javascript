535d59ea8249ee154c8e78a24fcaa47bb677f350
yamatowani <festajaune.18637@icloud.com>
2025-05-05 15:39:58 +0900

    add: rangeWithStep.js

--- exercise04/range.js
+++ exercise04/range.js
@@ -8,4 +8,24 @@ function range(start, end) {
   return result
 }
 
-console.log(range(1,10))
+console.log(range(1, 10))
+
+function rangeWithStep(start, end, step) {
+  let result = []
+  if (start < end) {
+    while (start < end + 1) {
+      result.push(start)
+      start += step
+    }
+    return result
+  } else {
+    while (end < start + 1) {
+      result.push(start)
+      start += step
+    }
+    return result
+  }
+}
+
+console.log(rangeWithStep(1, 10, 2))
+console.log(rangeWithStep(5, 2, -1))
