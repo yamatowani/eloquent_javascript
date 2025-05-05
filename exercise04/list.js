4f9bc45f6b00af67fd7d8c0433b54b65427f4d26
yamatowani <festajaune.18637@icloud.com>
2025-05-05 16:21:24 +0900

    add: ListToArray

--- exercise04/list.js
+++ exercise04/list.js
@@ -1,12 +1,20 @@
 function arrayToList(array) {
   let list = null
   for (let i = array.length - 1; i >= 0; i--) {
-     list = {
-      value: array[i],
-      rest: list     
-    }
+     list = { value: array[i], rest: list }
   }
   return list
 }
 
-console.log(arrayToList([1,2,3]))
+function listToArray(list) {
+  let array = []
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array
+}
+
+function prepend(value, list) {
+  return { value, list}
+}
+
